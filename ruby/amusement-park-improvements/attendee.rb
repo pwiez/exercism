d1bda9e5c0587be278de2d8424e64ab88323bb20
Pedro Wiezel <pwiezel@outlook.com>
2024-09-07 17:49:48 -0300

    amusement-park-improvements exercise complete! Concept: boolean values

---
+++ ruby/amusement-park-improvements/attendee.rb
@@ -0,0 +1,32 @@
+class Attendee
+  def initialize(height)
+    @height = height
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = pass_id
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+
+  # Do not edit above methods, add your own methods below.
+
+  def has_pass?
+    @pass_id == nil ? false : true
+  end
+
+  def fits_ride?(ride_minimum_height)
+    @height >= ride_minimum_height ? \
+               true : false
+  end
+
+  def allowed_to_ride?(ride_minimum_height)
+    if has_pass? and fits_ride?(ride_minimum_height)
+      true
+    else
+      false
+    end
+  end
+end
