b15a6a4aae3e9644a71ce608e7a885db0dd67026
Pedro Fernando Wiezel <pwiezel@outlook.com>
2024-09-08 16:26:12 -0300

    Syntax improvements

--- ruby/amusement-park-improvements/attendee.rb
+++ ruby/amusement-park-improvements/attendee.rb
@@ -14,16 +14,15 @@ class Attendee
   # Do not edit above methods, add your own methods below.
 
   def has_pass?
-    @pass_id == nil ? false : true
+    @pass_id.nil? ? false : true
   end
 
   def fits_ride?(ride_minimum_height)
-    @height >= ride_minimum_height ? \
-               true : false
+    @height >= ride_minimum_height
   end
 
   def allowed_to_ride?(ride_minimum_height)
-    if has_pass? and fits_ride?(ride_minimum_height)
+    if has_pass? && fits_ride?(ride_minimum_height)
       true
     else
       false
